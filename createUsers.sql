628c979688540576b0e3c14747576d02ea152cdb
Christie <mysteriousandspooky@gmail.com>
2021-02-18 20:22:47 -0500

    Added CREATE DATABASE sql

--- createUsers.sql
+++ createUsers.sql
@@ -1,3 +1,15 @@
+/* 
+Author: Christie Trent
+Date:   02.18.2021
+Notes:  MySQL script to create database and table
+Revisions:
+- 02.18.2021 - ctrent - Added CREATE DATABASE script
+*/
+
+CREATE DATABASE alpine;
+
+USE alpine;
+
 CREATE TABLE users (
     userID int,
     lastName varchar(255),
